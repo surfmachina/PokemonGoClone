899f032d315fc2836f667d11cea3350aae49b678
Thomas Carlson <surfmachina@gmail.com>
2019-04-21 01:08:55 +1000

    Working X4

--- PokemonGoClone/ViewController.swift
+++ PokemonGoClone/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var MapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var update_count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go")
+            MapView.showsUserLocation = true
+            
+            manager.startUpdatingLocation()
+            
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("We made it")
+         let region = MKCoordinateRegion.init(center: manager.location!.coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+        
+        if update_count < 3 {
+        MapView.setRegion(region, animated: false)
+            update_count += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
+    }
 }
 
